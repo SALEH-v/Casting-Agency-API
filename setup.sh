96d79931aa316afeca06fed3189ef36832dd2546
SALEH <sa.v-@hotmail.com>
2021-08-29 22:30:25 +0300

    c

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 export DATABASE_URL='postgresql://postgres:5399@localhost:5432/Casting_Agency'
-export AUTH0_DOMAIN='saleh-csft.us.auth0.com'
+export AUTH0_DOMAIN='saleh-dev.us.auth0.com'
 export TEST_DATABASE_URL='postgresql://postgres:5399@localhost:5432/Casting_Agency_Test'
 export FLASK_APP=app
 export FLASK_DEBUG=True
